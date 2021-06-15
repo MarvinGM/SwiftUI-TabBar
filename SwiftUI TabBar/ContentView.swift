38d6f257ca56126f377c5f4285c79f5fec7935d3
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-15 11:51:02 +0800

    Version 1.0

--- SwiftUI TabBar/ContentView.swift
+++ SwiftUI TabBar/ContentView.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.red
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue
+            }
+            .navigationTitle("Settings")
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+            
+            Text("Car")
+                .tabItem {
+                    Image(systemName: "car")
+                    Text("Car")
+                }
+        }
     }
 }
 
