cc126185f1918ce27d3677ed4de8d5d2c527da77
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-15 11:38:29 +0800

    Initial Commit

---
+++ SwiftUI TabBar/SwiftUI_TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabBarApp.swift
+//  SwiftUI TabBar
+//
+//  Created by Amben on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
